b95e21d2e39b55ce468e5c6173b3a2cf4ccb2203
Burak Çavuşoğlu <burakcavusoglu@Burak-MacBook-Pro.local>
2014-06-05 00:10:28 +0300

    Initial Commit

---
+++ Dusor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dusor
+//
+//  Created by Burak Çavuşoğlu on 05/06/14.
+//  Copyright (c) 2014 Burak Çavuşoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
